3bd20e37c153696a12626421113816be9236d899
meraj2085 <samirmeraj60@gmail.com>
2022-10-22 20:07:20 +0600

    adding travel bd

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-const hotels = require('./data/hotels.json')
-
+const touristSpots = require("./data/touristSpots.json");
 
 app.get("/", (req, res) => {
   res.send("Travel BD server is running...");
 });
 
-app.get('/hotels', (req, res)=>{
-     res.send(hotels)
-})
+app.get("/spots", (req, res) => {
+  res.send(touristSpots);
+});
 
+app.get("/spots/:id", (req, res) => {
+  const id = req.params.id;
 
+  const selectedSpot = touristSpots.find((h) => h.id === id);
+  res.send(selectedSpot);
+});
 
 app.listen(port, () => {
   console.log(`Travel BD server is running on port ${port}`);
